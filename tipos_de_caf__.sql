4c8f3e8def078ef9bfee2a067ead6672753da885
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2023-05-08 13:22:33 -0300

    ajustes no site

---
+++ tipos_de_caf__.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tempo de Geração: 03/05/2023 às 21h58min
+-- Versão do Servidor: 5.5.20
+-- Versão do PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `cafe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipos de café`
+--
+
+CREATE TABLE IF NOT EXISTS `tipos de café` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
